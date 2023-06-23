82e438fe94a4b09f807e463e7d2fbf6827f12c54
ayo-isaac <idowu.ayodejiisaac@gmail.com>
2023-06-22 17:56:57 -0700

    0-main.c, main.h

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void positive_or_negative(int i)
+{
+if (i < 0)
+{
+printf("%d is %s\n", i, "negative");
+}
+else if (i > 0)
+{
+printf("%d is %s\n", i, "positive");
+}
+else
+{
+printf("%d is %s\n", i, "zero");
+}
+return;
+}
