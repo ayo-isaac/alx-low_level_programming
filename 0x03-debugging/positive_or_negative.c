156585fe67d369ac9a583fffb4e1645c91194b59
ayo-isaac <idowu.ayodejiisaac@gmail.com>
2023-06-22 21:59:02 -0700

    0-main.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -8,17 +8,17 @@
 
 void positive_or_negative(int i)
 {
-if (i < 0)
-{
-printf("%d is %s\n", i, "negative");
-}
-else if (i > 0)
-{
-printf("%d is %s\n", i, "positive");
-}
-else
-{
-printf("%d is %s\n", i, "zero");
-}
-return;
+	if (i < 0)
+	{
+		printf("%d is %s\n", i, "negative");
+	}
+	else if (i > 0)
+	{
+		printf("%d is %s\n", i, "positive");
+	}
+	else
+	{
+	printf("%d is %s\n", i, "zero");
+	}
+		return;
 }
